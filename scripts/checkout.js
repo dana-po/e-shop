d41256304d5942cdad90304134cd016506299f58
dana-po <133027735+dana-po@users.noreply.github.com>
2024-01-18 17:05:23 +0200

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
         }
     });
     // console.log(matchingProduct);
-    cartSummaryHTML += `<div class="cart-item-container">
+    cartSummaryHTML += `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -104,5 +104,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
     //  console.log(productId);
     removeFromCart(productId)
     // console.log(cart);
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    // console.log(container);
+    container.remove();
     })
 })
