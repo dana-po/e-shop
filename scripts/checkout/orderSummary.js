2fa147e63fef44f1005112ace703e692535e7d22
dana-po <133027735+dana-po@users.noreply.github.com>
2024-01-19 17:43:23 +0200

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -7,12 +7,7 @@ import {
   deliveryOptions,
   getDeliveryOption,
 } from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import {renderPaymentSummary} from './paymentSummary.js'
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -122,6 +117,8 @@ export function renderOrderSummary() {
       );
       // console.log(container);
       container.remove();
+      renderPaymentSummary();
+
     });
   });
   document.querySelectorAll('.js-delivery-option').forEach((element) => {
@@ -129,6 +126,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
 });
 }
