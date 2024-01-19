3c2b32533acd8e3439a5629440d0bd5cd829d7aa
dana-po <133027735+dana-po@users.noreply.github.com>
2024-01-19 13:53:19 +0200

    regenerate HTML when updating delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -10,6 +10,7 @@ const today = dayjs();
 const deliveryDate = today.add(7, 'days');
 console.log(deliveryDate.format('dddd, MMMM D'));
 
+function renderOrderSummary(){
 let cartSummaryHTML = '';
 
 cart.forEach((cartItem) => {
@@ -131,5 +132,8 @@ document.querySelectorAll('.js-delivery-option').forEach((element)=>{
 element.addEventListener('click', ()=>{
   const{productId,deliveryOptionId}=element.dataset
 updateDeliveryOption(productId,deliveryOptionId)
+renderOrderSummary();
 })
 })
+}
+renderOrderSummary();
